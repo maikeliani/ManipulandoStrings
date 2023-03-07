d45a4cf97c2c374d60ef090022af1e0ab9d92102
maikeliani <maikel.iani31@gmail.com>
2023-03-07 15:30:47 -0300

    Add project files.

---
+++ ManipulandoStrings/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        string nome;
+        Console.WriteLine("Informe seu nome: ");
+        nome = Console.ReadLine();
+        /*
+        Console.WriteLine("Olá {0}", nome);
+        Console.WriteLine(nome.Length);
+        Console.WriteLine(nome.ToUpper());
+        Console.WriteLine(nome.Trim('l')); // remove caracteres de INICIO e FIM
+        Console.ReadLine();
+        */
+
+        RemoveVogais(nome);
+       
+
+        void RemoveVogais(string x)
+        {
+            
+            x = x.Replace('a', '\0');
+            x = x.Replace('e', '\0');
+            x = x.Replace('i', '\0');
+            x = x.Replace('o', '\0');
+            x = x.Replace('u', '\0');
+
+            Console.WriteLine(x);
+           
+        }
+
+       
+
+
+
+    }
+}
